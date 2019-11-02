8b130605b609ed00ecbf3190619b9b6bc3856f5a
songwei163 <songweicpp@163.com>
2019-11-02 09:59:29 +0800

    add BinaryTree

--- dataStucture/Tree/BinaryTree/BinaryTree.h
+++ dataStucture/Tree/BinaryTree/BinaryTree.h
@@ -5,6 +5,13 @@
 #ifndef SOMEDEOMSFORINTERVIEW_DATASTUCTURE_TREE_BINARYTREE_BINARYTREE_H_
 #define SOMEDEOMSFORINTERVIEW_DATASTUCTURE_TREE_BINARYTREE_BINARYTREE_H_
 
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <queue>
+
+using namespace std;
+
 template<typename T>
 struct BinTreeNode {
   T data;
@@ -27,7 +34,6 @@ class BinaryTree {
     }
   }
   ~BinaryTree() { destory(root); }
-
  public:
   bool IsEmpty() const { return root == nullptr; }
   BinTreeNode<T> *Parent(const BinTreeNode<T> *current) const;
@@ -37,25 +43,51 @@ class BinaryTree {
   BinTreeNode<T> *RightChild(const BinTreeNode<T> *current) const {
     return current != nullptr ? current->rightChild : nullptr;
   }
+  bool Find(const T &e) const { return Find(root, e); }
   int Height() const { return Height(root); }
   int Size() const { return Size(root); }
   BinTreeNode<T> *getRoot() const { return root; }
-  void preOrder(void (*visit)(BinTreeNode<T> *ptr)) const { preOrder(root, visit); }
-  void inOrder(void (*visit)(BinTreeNode<T> *ptr)) const { preOrder(root, visit); }
-  void postOrder(void (*visit)(BinTreeNode<T> *ptr)) const { preOrder(root, visit); }
-  void levelOrder(void (*visit)(BinTreeNode<T> *ptr)) const;
-
+  void PreOrder(void (*visit)(const BinTreeNode<T> *ptr)) const { PreOrder(root, visit); }
+  void InOrder(void (*visit)(const BinTreeNode<T> *ptr)) const { PreOrder(root, visit); }
+  void PostOrder(void (*visit)(const BinTreeNode<T> *ptr)) const { PreOrder(root, visit); }
+  void levelOrder(void (*visit)(const BinTreeNode<T> *ptr)) const;
+  bool Insert(const T &e) { return Insert(root, e); }
+ public:
+  void printBinTree(ostream &out = cout) const { printBinTree(root, out); }
+  void Output(ostream &out = cout) const {
+    out << "The structure of the binary tree is:\n";
+    Output(root, string(" "), out);
+    out << endl;
+  }
+  void PreOrder1(void (*visit)(const BinTreeNode<T> *ptr)) const;
+  void InOrder1(void (*visit)(const BinTreeNode<T> *ptr)) const;
+  void PostOrder1(void (*visit)(const BinTreeNode<T> *ptr)) const;
+  void CreateCompBinTree(T *CBT, int num) { CreateCompBinTree(CBT, num, 0, root); }
+  friend istream &operator>>(istream &in, BinaryTree<T> &Tree) {
+    Tree.CreateBinTree(in, Tree.root);
+    return in;
+  }
+  friend ostream &operator<<(ostream &out, BinaryTree<T> &Tree) {
+    Tree.printBinTree(out);
+    out << endl;
+    return out;
+  }
  private:
+  void CreateBinTree(istream &in, BinTreeNode<T> *&subTree);
+  void CreateCompBinTree(T *CBT, int num, int rt, BinTreeNode<T> *&subTree);
+  void Traverse(const BinTreeNode<T> *subTree, ostream &out) const;
+  void printBinTree(const BinTreeNode<T> *subTree, ostream &out = cout) const;
+  void Output(BinTreeNode<T> *subTree, const string &str, ostream &out) const;
+  bool Find(const BinTreeNode<T> *subTree, const T &e) const;
   BinTreeNode<T> *Copy(const BinTreeNode<T> *r) const;
   BinTreeNode<T> *Parent(const BinTreeNode<T> *subTree, BinTreeNode<T> *current) const;
   void destory(BinTreeNode<T> *subTree);
   int Height(BinTreeNode<T> *subTree) const;
   int Size(BinTreeNode<T> *subTree) const;
-  void preOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTreeNode<T> *ptr)) const;
-  void inOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTreeNode<T> *ptr)) const;
-  void postOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTreeNode<T> *ptr)) const;
-  int Insert(const T &item);
-  BinTreeNode<T> *Find(T &item) const;
+  void PreOrder(const BinTreeNode<T> *subTree, void (*visit)(const BinTreeNode<T> *ptr)) const;
+  void InOrder(const BinTreeNode<T> *subTree, void (*visit)(const BinTreeNode<T> *ptr)) const;
+  void PostOrder(const BinTreeNode<T> *subTree, void (*visit)(const BinTreeNode<T> *ptr)) const;
+  bool Insert(BinTreeNode<T> *subTree, const T &e);
  private:
   BinTreeNode<T> *root;
   T RefValue;
@@ -71,16 +103,7 @@ BinTreeNode<T> *BinaryTree<T>::Parent(const BinTreeNode<T> *current) const {
 }
 
 template<typename T>
-int BinaryTree<T>::Insert(const T &item) {
-  return 0;
-}
-
-template<typename T>
-BinTreeNode<T> *BinaryTree<T>::Find(T &item) const {
-  return nullptr;
-}
-template<typename T>
-void BinaryTree<T>::postOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTreeNode<T> *)) const {
+void BinaryTree<T>::PostOrder(const BinTreeNode<T> *subTree, void (*visit)(const BinTreeNode<T> *)) const {
   if (subTree != nullptr) {
     PostOrder(subTree->leftChild, visit);
     PostOrder(subTree->rightChild, visit);
@@ -89,7 +112,7 @@ void BinaryTree<T>::postOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTr
 }
 
 template<typename T>
-void BinaryTree<T>::inOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTreeNode<T> *)) const {
+void BinaryTree<T>::InOrder(const BinTreeNode<T> *subTree, void (*visit)(const BinTreeNode<T> *)) const {
   if (subTree != nullptr) {
     InOrder(subTree->leftChild, visit);
     visit(subTree);
@@ -98,7 +121,7 @@ void BinaryTree<T>::inOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTree
 }
 
 template<typename T>
-void BinaryTree<T>::preOrder(const BinTreeNode<T> &subTree, void (*visit)(BinTreeNode<T> *)) const {
+void BinaryTree<T>::PreOrder(const BinTreeNode<T> *subTree, void (*visit)(const BinTreeNode<T> *)) const {
   if (subTree != nullptr) {
     visit(subTree);
     PreOrder(subTree->leftChild, visit);
@@ -127,8 +150,24 @@ int BinaryTree<T>::Height(BinTreeNode<T> *subTree) const {
 }
 
 template<typename T>
-void BinaryTree<T>::levelOrder(void (*visit)(BinTreeNode<T> *)) const {
-
+void BinaryTree<T>::levelOrder(void (*visit)(const BinTreeNode<T> *)) const {
+  if (root == nullptr) return;
+  queue<BinTreeNode<T> *> Q;
+  BinTreeNode<T> *p = root;
+  visit(p);
+  Q.push(p);
+  while (!Q.empty()) {
+    p = Q.front();
+    Q.pop();
+    if (p->leftChild != nullptr) {
+      visit(p->leftChild);
+      Q.push(p->leftChild);
+    }
+    if (p->rightChild != nullptr) {
+      visit(p->rightChild);
+      Q.push(p->rightChild);
+    }
+  }
 }
 
 template<typename T>
@@ -166,6 +205,123 @@ BinTreeNode<T> *BinaryTree<T>::Copy(const BinTreeNode<T> *r) const {
   ptr->leftChild = Copy(r->leftChild);
   ptr->rightChild = Copy(r->rightChild);
   return ptr;
+}
+
+template<typename T>
+bool BinaryTree<T>::Find(const BinTreeNode<T> *subTree, const T &e) const {
+  if (subTree == nullptr) {
+    return false;
+  }
+
+  if (subTree->data == e) {
+    return true;
+  }
+
+  bool p = false;
+  p = Find(subTree->leftChild, e);
+  if (p) {
+    return true;
+  } else {
+    return Find(subTree->rightChild, e);
+  }
+}
+
+template<typename T>
+bool BinaryTree<T>::Insert(BinTreeNode<T> *subTree, const T &e) {
+  if (subTree == nullptr) {
+    subTree = new BinTreeNode<T>(e);
+    assert(subTree);
+    return true;
+  } else {
+    return Insert(subTree->leftChild, e) || Insert(subTree->rightChild, e);
+  }
+}
+
+template<typename T>
+void BinaryTree<T>::printBinTree(const BinTreeNode<T> *subTree, ostream &out) const {
+  if (subTree != nullptr) {
+    out << subTree->data;
+    if (subTree->leftChild || subTree->rightChild) {
+      out << "(";
+      printBinTree(subTree->leftChild, out);
+      if (subTree->rightChild) {
+        out << ",";
+      }
+      printBinTree(subTree->rightChild, out);
+      out << ")";
+    }
+  }
+}
+
+template<typename T>
+void BinaryTree<T>::Output(BinTreeNode<T> *subTree, const string &str, ostream &out) const {
+  if (subTree == nullptr) {
+    return;
+  }
+  out << str << subTree->data;
+  if (subTree->leftChild) {
+    out << "©¤©´" << endl;
+    if (subTree->rightChild) {
+      Output(subTree->leftChild, str + "©¦¡¡", out);
+    } else {
+      Output(subTree->leftChild, str + "¡¡¡¡", out);
+    }
+  }
+  if (subTree->rightChild) {
+    out << endl << str << "©¸©¤©´" << endl;
+    Output(subTree->rightChild, str + "¡¡¡¡", out);
+  }
+}
+
+template<typename T>
+void BinaryTree<T>::PostOrder1(void (*visit)(const BinTreeNode<T> *)) const {
+
+}
+
+template<typename T>
+void BinaryTree<T>::InOrder1(void (*visit)(const BinTreeNode<T> *)) const {
+
+}
+
+template<typename T>
+void BinaryTree<T>::PreOrder1(void (*visit)(const BinTreeNode<T> *)) const {
+
+}
+
+template<typename T>
+void BinaryTree<T>::Traverse(const BinTreeNode<T> *subTree, ostream &out) const {
+  if (subTree != nullptr) {
+    out << subTree->data << ' ';
+    Traverse(subTree->leftChild, out);
+    Traverse(subTree->rightChild, out);
+  }
+}
+
+template<typename T>
+void BinaryTree<T>::CreateCompBinTree(T *CBT, int num, int rt, BinTreeNode<T> *&subTree) {
+  if (rt >= num) {
+    subTree = nullptr;
+  } else {
+    subTree = new BinTreeNode<T>(CBT[rt]);
+    assert(subTree != nullptr);
+    CreateCompBinTree(CBT, num, 2 * rt + 1, subTree->leftChild);
+    CreateCompBinTree(CBT, num, 2 * rt + 2, subTree->rightChild);
+  }
+}
+
+template<typename T>
+void BinaryTree<T>::CreateBinTree(istream &in, BinTreeNode<T> *&subTree) {
+  T item;
+  if (in >> item) {
+    if (item != RefValue) {
+      subTree = new BinTreeNode<T>(item);
+      assert(subTree);
+      CreateBinTree(in, subTree->leftChild);
+      CreateBinTree(in, subTree->rightChild);
+    } else {
+      subTree = nullptr;
+    }
+  }
 }
 
 #endif //SOMEDEOMSFORINTERVIEW_DATASTUCTURE_TREE_BINARYTREE_BINARYTREE_H_
--- dataStucture/Tree/BinaryTree/testBinaryTree.cpp
+++ dataStucture/Tree/BinaryTree/testBinaryTree.cpp
@@ -2,3 +2,47 @@
 // Created by S on 2019/10/31.
 //
 
+#include "BinaryTree.h"
+#include <fstream>
+#include <iomanip>
+
+void visit(const BinTreeNode<int> *t) {
+  cout << t->data << "   ";
+}
+
+void test01();
+
+int main() {
+  test01();
+}
+
+void test01() {
+  ifstream fin("data.txt");
+  assert(fin);
+  BinaryTree<int> binTree(0);
+  assert(fin >> binTree);
+  fin.close();
+  cout << "The binary tree is: \n" << binTree << endl;
+  binTree.Output();
+  ofstream fout("output.txt");
+  assert(fout);
+  binTree.Output(fout);
+  fout.close();
+  BinaryTree<int> binTree1(binTree);
+  cout << "The copy binary tree is: \n" << binTree1 << endl;
+  binTree1.Output();
+
+  cout << "\nThe preorder of the binary tree is:\n";
+  binTree.PreOrder(visit);
+  cout << "\n\nThe inorder of the binary tree is:\n";
+  binTree.InOrder(visit);
+  cout << "\n\nThe postorder of the binary tree is:\n";
+  binTree.PostOrder(visit);
+  cout << "\n\nThe levelOrder of the binary tree is:\n";
+  binTree.levelOrder(visit);
+  cout << "\n\nThe height of the binary tree is:\n";
+  cout << binTree.Height();
+  cout << "\n\nThe Size of the binary tree is:\n";
+  cout << binTree.Size();
+  cout << endl << endl;
+}
