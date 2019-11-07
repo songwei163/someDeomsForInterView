07a9e953dd8564fb0518af9130ff4b88f1b5308e
songwei163 <songweicpp@163.com>
2019-11-07 14:21:59 +0800

    ——

--- cmake-build-debug/dataStucture/LinerList/cmake_install.cmake
+++ cmake-build-debug/dataStucture/LinerList/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/s/my_work_space/someDeomsForInterView/dataStucture/LinerList
+# Install script for directory: C:/Users/S/CLionProjects/someDeomsForInterView/dataStucture/LinerList
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/someDeomsForInterView")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -39,11 +34,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/s/my_work_space/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/SeqList/cmake_install.cmake")
-  include("/home/s/my_work_space/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/LinkedList/cmake_install.cmake")
-  include("/home/s/my_work_space/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/StaticList/cmake_install.cmake")
-  include("/home/s/my_work_space/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/CircList/cmake_install.cmake")
-  include("/home/s/my_work_space/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/DblList/cmake_install.cmake")
+  include("C:/Users/S/CLionProjects/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/SeqList/cmake_install.cmake")
+  include("C:/Users/S/CLionProjects/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/LinkedList/cmake_install.cmake")
+  include("C:/Users/S/CLionProjects/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/StaticList/cmake_install.cmake")
+  include("C:/Users/S/CLionProjects/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/CircList/cmake_install.cmake")
+  include("C:/Users/S/CLionProjects/someDeomsForInterView/cmake-build-debug/dataStucture/LinerList/DblList/cmake_install.cmake")
 
 endif()
 
