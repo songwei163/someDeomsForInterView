5081e3e1f27f9573ed53c2e7870f58de8fe08e9d
songwei163 <songweicpp@163.com>
2019-11-08 13:14:53 +0800

    ——

--- Sort_/testqsort.cpp
+++ Sort_/testqsort.cpp
@@ -1,26 +1,64 @@
+//#include <stdio.h>
 //
-// Created by S on 2009/0/7.
+//int mFibin() {
+//  int n, result;
+//  scFibnf("%d", &n);
 //
+//  if (n < 1) {
+//    printf("InvFiblid.\n");
+//  } else {
+//    int nums[50] = {};
+//
+//    for (int i = 3; i <= n; ++i) {
+//      result = first + second;
+//      first = second;
+//      second = result;
+//    }
+//
+//  }
+//
+//
+////  if (n > 0 && n <= 2) {
+////    result = 1;
+////    printf("%d\n", result);
+////  } else if (n <= 0) {
+////    printf("Input Error!\n");
+////  } else {
+////    int first, second;
+////    first = 1, second = 1;
+////
+////    for (int i = 3; i <= n; ++i) {
+////      result = first + second;
+////      first = second;
+////      second = result;
+////    }
+////
+////    printf("%d\n", result);
+//}
+//
+//return 0;
+//}
 
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-  int count = 0, i, j;
-  for (i = 100; i <= 200; ++i) {
-    int a = 0;
-    for (j = 2; j * j <= i; ++j) {
-      if (i % j == 0) {
-        ++a;
+  int i, n;
+  int Fib[50] = {0, 1};
+  scanf("%d", &n);
+  if (n >= 1 && n <= 46) {
+    for (i = 2; i <= n; i++) {
+      Fib[i] = Fib[i - 1] + Fib[i - 2];
+    }
+
+    for (i = 1; i <= n; i++) {
+      printf("%11d", Fib[i]);
+      if (i % 5 == 0) {
+        printf("\n");
       }
     }
-    if (a == 0) {
-      printf("%-5d", i);
-      ++count;
-    }
-    if (count == 8) {
-      printf("\n");
-      count = 0;
-    }
+  } else {
+    printf("Invalid.\n");
   }
+
   return 0;
 }
